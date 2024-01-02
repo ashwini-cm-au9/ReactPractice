45a45a275e1e4d5f49e889fb935784c1669b2390
ashwini-cm-au9 <ashwinichuri93@gmail.com>
2024-01-02 22:50:48 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+const heading=React.createElement('h1',{id:'heading',color:'red'},"This is the second Heading")
+
+const root=ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(heading)
+
+const JSX=<h1>This is JSX</h1>
+
+root.render(JSX)
